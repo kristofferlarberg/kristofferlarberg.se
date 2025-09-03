303cc62297001f9c9c576e1aca035e8e5f53e1ff
Kristoffer Larberg <51208557+kristofferlarberg@users.noreply.github.com>
2025-09-03 17:25:56 +0200

    use filter buttons for categories

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function createFilterButtons() {
-  const filters = document.querySelector(".filters");
+  const mainFilters = document.querySelector(".main-filters");
+  const categoryFilters = document.querySelector(".category-filters");
+  const filtersContainer = document.querySelector(".filters-container");
   const buttons = document.querySelectorAll("button");
   const skills = document.querySelectorAll(".skill");
 
   // Function to check width and show/hide filters
   function checkWidthAndShowFilters() {
     if (window.innerWidth >= 250) {
-      filters.style.display = "flex";
+      filtersContainer.style.display = "flex";
     } else {
-      filters.style.display = "none";
+      filtersContainer.style.display = "none";
     }
   }
 
@@ -25,12 +27,17 @@ function createFilterButtons() {
         b.classList.remove("active");
         b.setAttribute("aria-pressed", "false");
       });
-      button.classList.add("active");
+
+      if (button.dataset.filter !== "all") {
+        button.classList.add("active");
+      }
       button.setAttribute("aria-pressed", "true");
 
       const filter = button.dataset.filter;
+      console.log(filter);
 
       skills.forEach((skill) => {
+        console.log(skill.dataset.category);
         if (filter === "all") {
           skill.classList.remove("hidden");
         } else if (filter === "taught" && skill.dataset.taught === "true") {
@@ -40,6 +47,31 @@ function createFilterButtons() {
         } else if (
           filter === "collaborated" &&
           skill.dataset.collaborated === "true"
+        ) {
+          skill.classList.remove("hidden");
+        } else if (
+          filter === "languages" &&
+          skill.dataset.category === "languages"
+        ) {
+          skill.classList.remove("hidden");
+        } else if (
+          filter === "frameworks-libraries" &&
+          skill.dataset.category === "frameworks-libraries"
+        ) {
+          skill.classList.remove("hidden");
+        } else if (
+          filter === "backend-data" &&
+          skill.dataset.category === "backend-data"
+        ) {
+          skill.classList.remove("hidden");
+        } else if (
+          filter === "testing" &&
+          skill.dataset.category === "testing"
+        ) {
+          skill.classList.remove("hidden");
+        } else if (
+          filter === "tooling" &&
+          skill.dataset.category === "tooling"
         ) {
           skill.classList.remove("hidden");
         } else {
