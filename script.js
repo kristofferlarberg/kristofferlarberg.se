c90e884af3122fe8349f6f8ac3631eab988ff688
kristofferlarberg <kristoffer.larberg@icloud.com>
2025-09-07 16:54:35 +0200

    separate skills filtering

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-function filterButtons() {
-  const mainFilters = document.querySelector(".main-filters");
-  const categoryFilters = Array.from(
-    document.querySelectorAll(".category-filters")
-  );
-  const filtersContainer = document.querySelectorAll(".filters-container");
-  const buttons = document.querySelectorAll("button");
-  const skills = document.querySelectorAll(".skill");
-  const technicalSkills = document.querySelectorAll(".skill.technical");
-  const processSkills = document.querySelectorAll(".skill.process");
+const mainFilters = document.querySelector(".main-filters");
+const technicalCategoryFilters = Array.from(
+  document.querySelectorAll(".technical-category-filters")
+);
+const processCategoryFilters = Array.from(
+  document.querySelectorAll(".process-category-filters")
+);
+const filtersContainer = document.querySelectorAll(".filters-container");
+const technicalSkillsFilterbuttons = document.querySelectorAll(
+  ".technical-skills-filter"
+);
+const processSkillsFilterButtons = document.querySelectorAll(
+  ".process-skills-filter"
+);
+const technicalSkills = document.querySelectorAll(".skill.technical");
+const processSkills = document.querySelectorAll(".skill.process");
+const skills = document.querySelectorAll(".skill");
 
-  function checkWidthAndShowFilters() {
-    if (window.innerWidth >= 250) {
-      filtersContainer.forEach((element) => {
-        element.style.display = "flex";
-      });
-    } else {
-      filtersContainer.forEach((element) => {
-        element.style.display = "none";
-      });
-    }
+function checkWidthAndShowFilters() {
+  if (window.innerWidth >= 250) {
+    filtersContainer.forEach((element) => {
+      element.style.display = "flex";
+    });
+  } else {
+    filtersContainer.forEach((element) => {
+      element.style.display = "none";
+    });
   }
+}
 
-  checkWidthAndShowFilters();
+checkWidthAndShowFilters();
 
-  window.addEventListener("resize", checkWidthAndShowFilters);
-
+window.addEventListener("resize", checkWidthAndShowFilters);
+function buttonListener(buttons) {
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       const filter = button.dataset.filter;
@@ -40,10 +47,17 @@ function filterButtons() {
         return;
       }
 
-      const isMainFilter = mainFilters.contains(button);
-      const isCategoryFilter = categoryFilters.some((filter) =>
+      const isMainFilter =
+        button.classList.contains("technical-skills-filter") &&
+        mainFilters.contains(button);
+
+      const isProcessCategoryFilter = processCategoryFilters.some((filter) =>
         filter.contains(button)
       );
+      const isTechnicalCategoryFilter = technicalCategoryFilters.some(
+        (filter) => filter.contains(button)
+      );
+
       const isCurrentlyActive = button.classList.contains("active");
 
       if (isCurrentlyActive) {
@@ -55,8 +69,15 @@ function filterButtons() {
             b.classList.remove("active");
             b.setAttribute("aria-pressed", "false");
           });
-        } else if (isCategoryFilter) {
-          categoryFilters.forEach((filter) => {
+        } else if (isTechnicalCategoryFilter) {
+          technicalCategoryFilters.forEach((filter) => {
+            filter.querySelectorAll("button").forEach((b) => {
+              b.classList.remove("active");
+              b.setAttribute("aria-pressed", "false");
+            });
+          });
+        } else if (isProcessCategoryFilter) {
+          processCategoryFilters.forEach((filter) => {
             filter.querySelectorAll("button").forEach((b) => {
               b.classList.remove("active");
               b.setAttribute("aria-pressed", "false");
@@ -68,88 +89,108 @@ function filterButtons() {
         button.setAttribute("aria-pressed", "true");
       }
 
-      applyFilters();
+      applyFilters(
+        button.classList.contains("technical-skills-filter")
+          ? "technical"
+          : "process"
+      );
     });
   });
+}
 
-  function applyFilters() {
-    const activeMainFilter = mainFilters.querySelector("button.active");
-    const activeCategoryFilter = categoryFilters
+function applyFilters(skillsType) {
+  if (skillsType === "process") {
+    const activeCategoryFilter = processCategoryFilters
       .find((filter) => filter.querySelector("button.active"))
       ?.querySelector("button.active");
 
-    if (
-      activeCategoryFilter &&
-      (activeCategoryFilter.dataset.filter === "practices" ||
-        activeCategoryFilter.dataset.filter === "process-tooling")
-    ) {
-      processSkills.forEach((skill) => {
-        let shouldShow = true;
+    processSkills.forEach((skill) => {
+      let shouldShow = true;
 
-        if (activeCategoryFilter && shouldShow) {
-          const categoryFilter = activeCategoryFilter.dataset.filter;
-          if (skill.dataset.category !== categoryFilter) {
-            shouldShow = false;
-          }
+      if (activeCategoryFilter && shouldShow) {
+        const categoryFilter = activeCategoryFilter.dataset.filter;
+        if (skill.dataset.category !== categoryFilter) {
+          shouldShow = false;
         }
+      }
 
-        if (shouldShow) {
-          skill.classList.remove("hidden");
-        } else {
-          skill.classList.add("hidden");
+      if (shouldShow) {
+        skill.classList.remove("hidden");
+      } else {
+        skill.classList.add("hidden");
+      }
+    });
+  } else if (skillsType === "technical") {
+    const activeMainFilter = mainFilters.querySelector("button.active");
+
+    const activeCategoryFilter = technicalCategoryFilters
+      .find((filter) => filter.querySelector("button.active"))
+      ?.querySelector("button.active");
+
+    technicalSkills.forEach((skill) => {
+      let shouldShow = true;
+
+      if (skillsType === "technical" && activeMainFilter) {
+        const mainFilter = activeMainFilter.dataset.filter;
+        if (mainFilter === "mentored" && skill.dataset.mentored !== "true") {
+          shouldShow = false;
+        } else if (mainFilter === "future" && skill.dataset.future !== "true") {
+          shouldShow = false;
+        } else if (
+          mainFilter === "collaborated" &&
+          skill.dataset.collaborated !== "true"
+        ) {
+          shouldShow = false;
         }
-      });
-    } else {
-      technicalSkills.forEach((skill) => {
-        if (skill.classList.contains("technical")) {
-          let shouldShow = true;
+      }
 
-          if (activeMainFilter) {
-            const mainFilter = activeMainFilter.dataset.filter;
-            if (
-              mainFilter === "mentored" &&
-              skill.dataset.mentored !== "true"
-            ) {
-              shouldShow = false;
-            } else if (
-              mainFilter === "mentored-process" &&
-              skill.dataset.mentoredProcess !== "true"
-            ) {
-              shouldShow = false;
-            } else if (
-              mainFilter === "mentored-process" &&
-              skill.dataset.mentoredProcess !== "true"
-            ) {
-              shouldShow = false;
-            } else if (
-              mainFilter === "future" &&
-              skill.dataset.future !== "true"
-            ) {
-              shouldShow = false;
-            } else if (
-              mainFilter === "collaborated" &&
-              skill.dataset.collaborated !== "true"
-            ) {
-              shouldShow = false;
-            }
-          }
-
-          if (activeCategoryFilter && shouldShow) {
-            const categoryFilter = activeCategoryFilter.dataset.filter;
-            if (skill.dataset.category !== categoryFilter) {
-              shouldShow = false;
-            }
-          }
-
-          if (shouldShow) {
-            skill.classList.remove("hidden");
-          } else {
-            skill.classList.add("hidden");
-          }
+      if (activeCategoryFilter && shouldShow) {
+        const categoryFilter = activeCategoryFilter.dataset.filter;
+        if (skill.dataset.category !== categoryFilter) {
+          shouldShow = false;
         }
-      });
-    }
+      }
+
+      if (shouldShow) {
+        skill.classList.remove("hidden");
+      } else {
+        skill.classList.add("hidden");
+      }
+    });
   }
+  return;
 }
 
-filterButtons();
+// function applyFilters(skillsType) {
+//   let shouldShow = true;
+
+//   if (skillsType === "technical" && activeMainFilter) {
+//     const mainFilter = activeMainFilter.dataset.filter;
+//     if (mainFilter === "mentored" && skill.dataset.mentored !== "true") {
+//       shouldShow = false;
+//     } else if (mainFilter === "future" && skill.dataset.future !== "true") {
+//       shouldShow = false;
+//     } else if (
+//       mainFilter === "collaborated" &&
+//       skill.dataset.collaborated !== "true"
+//     ) {
+//       shouldShow = false;
+//     }
+//   }
+
+//   if (activeCategoryFilter && shouldShow) {
+//     const categoryFilter = activeCategoryFilter.dataset.filter;
+//     if (skill.dataset.category !== categoryFilter) {
+//       shouldShow = false;
+//     }
+//   }
+
+//   if (shouldShow) {
+//     skill.classList.remove("hidden");
+//   } else {
+//     skill.classList.add("hidden");
+//   }
+// }
+
+buttonListener(technicalSkillsFilterbuttons);
+buttonListener(processSkillsFilterButtons);
